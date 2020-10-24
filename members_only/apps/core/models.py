cb2f2fc469e70214cfb7375e920a7e69a70a1c11
Ben Steel <bsteel300@gmail.com>
2020-10-23 18:05:36 -0700

    add Post model

--- members_only/apps/core/models.py
+++ members_only/apps/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+
+    class Meta:
+        ordering = ['-created_on']
