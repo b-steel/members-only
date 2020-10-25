b35092d2081d7bccc005c86d0d705e1c9dba780b
Ben Steel <bsteel300@gmail.com>
2020-10-25 16:40:24 -0700

    create frontpage view

--- members_only/apps/core/views.py
+++ members_only/apps/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
-# Create your views here.
+from .models import Post
+
+class FrontPage(View):
+    def get(self, request):
+        return render(request, 'core/frontpage.html', {})
