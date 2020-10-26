afce76a1ea01c0ae60fa1299d7abdcd5c4de00b6
Ben Steel <bsteel300@gmail.com>
2020-10-26 10:00:08 -0700

    add abilty to tag users in posts

--- members_only/apps/core/models.py
+++ members_only/apps/core/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     created_on = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
+    tags = models.ManyToManyField(User, related_name='tags')
 
     class Meta:
         ordering = ['-created_on']
--- members_only/apps/core/views.py
+++ members_only/apps/core/views.py
@@ -6,10 +6,44 @@ from django.views import View
 from .models import Post
 from .forms import PostForm
 
+import re
+
+def split_tagged_post(text):
+    p = re.compile('(@[\w_\-!]+)')
+    text = re.split(p, text)
+    return text
+
 class FrontPage(View):
     def get(self, request):
         posts = Post.objects.all()
-        return render(request, 'core/frontpage.html', {'posts': posts})
+
+        context = {
+            'posts':[]
+        }
+
+        for post in posts:
+            textlist = split_tagged_post(post.text)
+
+            tagged_text = []
+
+            for s in textlist:
+                if s and s[0]=='@':
+                    user = User.objects.get(username=s[1:])
+                    if user:
+                        tagged_text.append({'text': s, 'tag': user}) 
+                    else:
+                        tagged_text.append({'text': s, 'tag': None})
+                else:
+                    tagged_text.append({'text': s, 'tag': None})
+
+            context['posts'].append({'post': post, 'tagged_text': tagged_text})
+
+        return render(request, 'core/frontpage.html', context)
+
+def get_tags_or_none(text):
+    p = re.compile('(?<=@)\S+')
+    tags = re.findall(p, text)
+    return tags
 
 class PostView(LoginRequiredMixin, View):
     def get(self, request):
@@ -20,8 +54,20 @@ class PostView(LoginRequiredMixin, View):
         form = PostForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data['text']
+            tags = get_tags_or_none(text)
+
             post = Post(created_by=request.user, text=text)
             post.save()
+
+            users = []
+            for tag in tags:
+                user = User.objects.get(username=tag)
+                if user:
+                    users.append(user)
+            
+            post.tags.add(*users)
+            post.save()
+
             return redirect('frontpage')
 
 
