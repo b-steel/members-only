61c037014b2b002c6c2bbde72efb78bac664eb38
Ben Steel <bsteel300@gmail.com>
2020-10-25 18:00:01 -0700

    add profile model

--- members_only/apps/accounts/models.py
+++ members_only/apps/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, related_name='profile', on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+User.profile = property(lambda u: Profile.objects.get_or_create(user=u)[0
+])
